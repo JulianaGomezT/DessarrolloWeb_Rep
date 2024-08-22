d18a45d4752644f4f7912765891822d70caf5042
JulianaGomezT <jgomeztraverso@uade.edu.ar>
2024-08-22 13:54:57 -0300

    3

--- Mi Portfolio/contact_form.php
+++ Mi Portfolio/contact_form.php
@@ -1,30 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoge y limpia los datos del formulario
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "POST":
+        $to = "juliana.gomez.traverso@gmail.com";
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
 
-    // Configura los parámetros del correo
-    $to = 'juliana.gomez.traverso@gmail.com';
-    $subject = 'Nuevo mensaje de contacto';
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        $subject = "Consulta portfolio de $name";
+        $body = "Nombre: $name\n";
+        $body .= "Correo Electrónico: $email\n";
+        $body .= "Mensaje:\n$message";
 
-    // Cuerpo del mensaje
-    $body = "<p><strong>Nombre:</strong> $name</p>";
-    $body .= "<p><strong>Correo Electrónico:</strong> $email</p>";
-    $body .= "<p><strong>Mensaje:</strong></p>";
-    $body .= "<p>$message</p>";
+        $headers = "From: $email\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Envía el correo
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Gracias por tu mensaje. Te responderemos pronto.";
-    } else {
-        echo "Lo siento, hubo un problema al enviar tu mensaje.";
-    }
-} else {
-    echo "Método de solicitud no permitido.";
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Gracias por tu mensaje, $name. Nos pondremos en contacto contigo pronto.";
+        } else {
+            echo "Hubo un problema al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.";
+        }
+        break;
+
+    case "GET":
+        echo "Método GET no permitido para este formulario.";
+        http_response_code(405); 
+        break;
+
+    default:
+        echo "Método HTTP no soportado.";
+        http_response_code(405); 
+        break;
 }
-?>
